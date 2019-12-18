bd9356b944266abfe5db82d821664631e1c10ee4
danielab1 <49786814+danielab1@users.noreply.github.com>
2019-12-18 14:05:06 +0200

    Add files via upload

---
+++ StanforNLP.java
@@ -0,0 +1,83 @@
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import java.util.ArrayList;
+
+public class StanforNLP {
+
+    public static void analyzeReview(Review review){
+        review.setSentiment(findSentiment(review.getText()));
+        review.analyzeSarcasm();
+        review.setEntities(findEntities(review.getText()));
+
+
+    }
+
+    public static int findSentiment(String review) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP  sentimentPipeline =  new StanfordCoreNLP(props);
+
+        int mainSentiment = 0;
+        if (review!= null && review.length() > 0) {
+            int longest = 0;
+            Annotation annotation = sentimentPipeline.process(review);
+            for (CoreMap sentence : annotation
+                    .get(SentencesAnnotation.class)) {
+                Tree tree = sentence
+                        .get(SentimentCoreAnnotations.AnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+
+            }
+        }
+        return mainSentiment;
+    }
+
+
+    public static ArrayList<String> findEntities(String review){
+        Properties props = new Properties();
+        props.put("annotators", "tokenize , ssplit, pos, lemma, ner");
+        StanfordCoreNLP NERPipeline =  new StanfordCoreNLP(props);
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(review);
+
+        // run all Annotators on this text
+        NERPipeline.annotate(document);
+
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+        ArrayList<String> entities = new ArrayList<String>();
+        for(CoreMap sentence: sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+
+                // this is the text of the token
+                //String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(NamedEntityTagAnnotation.class);
+                entities.add(ne);
+            }
+        }
+        return entities;
+
+    }
+}
